c20620f77cdb549f60f5c47d473cea88c23938ca
EC2 Default User <ec2-user@ip-172-31-8-59.ap-northeast-1.compute.internal>
2024-06-21 18:28:57 +0000

    K28

--- app/controllers/admin/tagsearches_controller.rb
+++ app/controllers/admin/tagsearches_controller.rb
@@ -3,7 +3,7 @@ class Admin::TagsearchesController < ApplicationController
     @model = Post
     @word = params[:content]
     @posts = Post.where("category LIKE ?", "%#{@word}%")
+    @banners = Banner.all
     render "admin/tagsearches/tagsearch"
   end
-    @banners = Banner.all
 end
--- app/controllers/user/tagsearches_controller.rb
+++ app/controllers/user/tagsearches_controller.rb
@@ -3,8 +3,8 @@ class User::TagsearchesController < ApplicationController
     @model = Post
     @word = params[:content]
     @posts = Post.where("category LIKE ?", "%#{@word}%")
-    render "user/tagsearches/tagsearch"
     @new_post = Post.new
     @banners = Banner.all
+    render "user/tagsearches/tagsearch"
   end
 end
