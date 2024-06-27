5402cd493a9bc7d77f2b7343b999fadd3ccfe441
EC2 Default User <ec2-user@ip-172-31-8-59.ap-northeast-1.compute.internal>
2024-06-27 05:42:08 +0000

    finish

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,5 +3,4 @@ class Room < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
   has_many :users, through: :entries, source: :user
-  validates :messages, length: { minimum: 1, maximum: 200 }
 end
