4d5399293abb1a466c715a89dd9e1cd0376098ea
EC2 Default User <ec2-user@ip-172-31-8-59.ap-northeast-1.compute.internal>
2024-06-27 14:57:26 +0000

    br

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def wrap_text(text, line_width = 50)
+    return "" if text.blank?
+    sanitized_text = h(text) # HTMLエスケープしておく
+    
+    # 改行を挿入する正規表現を調整
+    wrapped_text = sanitized_text.gsub(/(.{1,#{line_width}})(?:\s+|\z)/, "\\1<br>")
+    
+    wrapped_text.html_safe
+  end
 end
