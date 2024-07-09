916f7e6927ab49d78501d0f7c7a24164c196b715
EC2 Default User <ec2-user@ip-172-31-8-59.ap-northeast-1.compute.internal>
2024-07-09 07:13:56 +0000

    vali

--- app/controllers/user/messages_controller.rb
+++ app/controllers/user/messages_controller.rb
@@ -1,13 +1,24 @@
 class User::MessagesController < ApplicationController
-  before_action :authenticate_user!, :only => [:create]
+  before_action :authenticate_user!, only: [:create]
 
   def create
-    if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
-      @message = Message.create!(params.require(:message).permit(:message,:user_id, :content, :room_id).merge(:user_id => current_user.id))
-      redirect_to "/rooms/#{@message.room_id}"
+    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
+      @message = Message.new(message_params)
+      @message.user_id = current_user.id
+
+      if @message.save
+        redirect_to "/rooms/#{@message.room_id}"
+      else
+        redirect_back(fallback_location: root_path)
+      end
     else
       redirect_back(fallback_location: root_path)
     end
   end
-  
+
+  private
+
+  def message_params
+    params.require(:message).permit(:message, :room_id)
+  end
 end
